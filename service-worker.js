9d49bb71ee51c1975c431b4b02898185da90c654
Singh <Ashutosh.Singh04@smgglobal.motherson.com>
2025-12-09 09:49:31 +0530

    Updated all files

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-// Ready
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => clients.claim());
+
+// No caching at all
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
+});
